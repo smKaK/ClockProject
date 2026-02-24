aae74489e632eac59d303d9347ab546ae0d46b0c
Smka <klyatskosemyon16@gmail.com>
2026-02-24 23:20:38 +0200

    Remove unused string.h include from main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,7 +22,6 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <stdio.h>
-#include <string.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
